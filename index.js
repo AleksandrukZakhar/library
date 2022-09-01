48516823ec105b8ecd706ed275cc1f38d6271dd1
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-09-01 13:40:37 +0300

    Add validation

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const pages = document.getElementById("pages");
 const read = document.getElementById("read");
 const submit = document.getElementById("submit");
 const booksContainer = document.querySelector(".books-container");
+const form = document.querySelector("form");
 
 add.addEventListener("click", () =>
   inputContainer.classList.replace("hide", "show")
@@ -83,3 +84,32 @@ submit.addEventListener("click", () => {
   pages.value = "";
   read.value = "";
 });
+
+const validate = (el, patternTitle, pattern) => {
+  if (title.validity.valueMissing) {
+    el.setCustomValidity("Fill this field");
+    el.reportValidity();
+  } else if (title.validity.tooShort) {
+    el.setCustomValidity("Title must be at least 4 characters long");
+    el.reportValidity();
+  } else if (title.validity.patternMismatch) {
+    el.setCustomValidity(
+      `${patternTitle} is invalid follow this example ${pattern}`
+    );
+    el.reportValidity();
+  } else title.setCustomValidity("");
+};
+
+title.addEventListener("input", () => {
+  validate(title, "Title", "Harry Potter and the Philosophers Stone");
+});
+
+author.addEventListener("input", () => {
+  validate(author, "Author's name", "J K Rowling");
+});
+
+pages.addEventListener("input", () => {
+  validate(pages, "Pages count", "319");
+});
+
+form.addEventListener("submit", (e) => e.preventDefault());
